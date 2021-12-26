c1bbf884217f53944e2b2b286213c2a99a4809a3
Bernd Brägelmann <bernd@braegelmann.net>
2021-12-26 12:04:07 +0100

    optimizing

--- kraken2ledger.py
+++ kraken2ledger.py
@@ -36,13 +36,13 @@ for index, row in df.iterrows():
             for index, value in subframe.iloc[1].items() :
                 print("\t; ",index,": ",value)
             if subframe.iloc[0]['fee'] == 0:
-                print("\t",options.krakenaccount,"\t\t",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['amount'],trim='-'),"\t= ",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['balance'],trim='-'))
+                print("\t",options.krakenaccount,"\t\t",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['amount'],trim='-'))
             else:
-                print("\t",options.krakenaccount,"\t= ",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['balance'],trim='-'))
+                print("\t",options.krakenaccount,"\t=",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['balance'],trim='-'))
             if subframe.iloc[1]['fee'] == 0:
-                print("\t",options.krakenaccount,"\t\t",subframe.iloc[1]['asset'],np.format_float_positional(subframe.iloc[1]['amount'],trim='-'),"\t= ",subframe.iloc[1]['asset'],np.format_float_positional(subframe.iloc[1]['balance'],trim='-'))
+                print("\t",options.krakenaccount,"\t\t",subframe.iloc[1]['asset'],np.format_float_positional(subframe.iloc[1]['amount'],trim='-'))
             else:
-                print("\t",options.krakenaccount,"\t= ",subframe.iloc[1]['asset'],np.format_float_positional(subframe.iloc[1]['balance'],trim='-'))
+                print("\t",options.krakenaccount,"\t=",subframe.iloc[1]['asset'],np.format_float_positional(subframe.iloc[1]['balance'],trim='-'))
             print("\t",options.feeaccount,"\t\t",subframe.iloc[0]['asset']," ",np.format_float_positional(subframe.iloc[0]['fee'],trim='-'))
             print("\t",options.feeaccount,"\t\t",subframe.iloc[1]['asset']," ",np.format_float_positional(subframe.iloc[1]['fee'],trim='-'))
 
@@ -51,7 +51,7 @@ for index, row in df.iterrows():
         print(date.strftime('%Y/%m/%d'),"* ",row['type'])
         for index, value in row.items():
             print("\t; ",index,": ",value)
-        print("\t",options.krakenaccount,"\t= ",row['asset'],np.format_float_positional(row['balance'],trim='-'))
+        print("\t",options.krakenaccount,"\t=",row['asset'],np.format_float_positional(row['balance'],trim='-'))
         print("\t",options.feeaccount,"\t\t",row['asset']," ",np.format_float_positional(row['fee'],trim='-'))
 
         if row['type']=='deposit':

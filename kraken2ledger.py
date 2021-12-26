c029ce8db632c1f7b0fe56a3d6713f3cbb2da090
Bernd Brägelmann <bernd@braegelmann.net>
2021-12-26 20:02:38 +0100

    reducing redundancy

--- kraken2ledger.py
+++ kraken2ledger.py
@@ -1,8 +1,27 @@
 import pandas as pd
 import numpy as np
+import math
 from dateutil.parser import parse
 from optparse import OptionParser
 
+def fledger(account,amount,asset,balance=float("NaN")):
+    if asset == "ZEUR":
+        asset = "EUR";
+    if asset == "ZUSD":
+        asset = "USD";
+    ll="  "+account+"  "+asset+" "
+    if options.decimalcomma:
+        ll=ll+np.format_float_positional(amount,trim='-').replace(".",",")
+    else: 
+        ll=ll+np.format_float_positional(amount,trim='-')
+    if not math.isnan(balance):
+        ll=ll+" = "+asset+" "
+        if options.decimalcomma:
+            ll=ll+np.format_float_positional(balance,trim='-').replace(".",",")
+        else:
+            ll=ll+np.format_float_positional(balance,trim='-')
+    print(ll)
+#    print("\t",options.krakenaccount,"\t\t",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['amount'],trim='-'))
 
 parser = OptionParser()
 parser.add_option("-i", "--infile", dest="infile",
@@ -15,6 +34,9 @@ parser.add_option("-f", "--feeaccount", dest="feeaccount",
                   help="Name of the fee account", default="Fee")
 parser.add_option("-t", "--transferaccount", dest="transferaccount",
                   help="Name of the transfer account", default="Holdingwallet")
+parser.add_option("-d", "--decimal-comma",
+                  action="store_true", dest="decimalcomma", default=False,
+                  help="use decimal comma for ledger output")
 
 (options, args) = parser.parse_args()
 
@@ -36,8 +58,10 @@ for index, row in df.iterrows():
             for index, value in subframe.iloc[1].items() :
                 print("\t; ",index,": ",value)
             if subframe.iloc[0]['fee'] == 0:
-                print("\t",options.krakenaccount,"\t\t",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['amount'],trim='-'))
+                fledger(options.krakenaccount,subframe.iloc[0]['amount'],subframe.iloc[0]['asset'],subframe.iloc[0]['balance'],)
+#                print("\t",options.krakenaccount,"\t\t",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['amount'],trim='-'))
             else:
+#                fledger(options.krakenaccount,subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['balance']
                 print("\t",options.krakenaccount,"\t=",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['balance'],trim='-'))
             if subframe.iloc[1]['fee'] == 0:
                 print("\t",options.krakenaccount,"\t\t",subframe.iloc[1]['asset'],np.format_float_positional(subframe.iloc[1]['amount'],trim='-'))
@@ -61,18 +85,4 @@ for index, row in df.iterrows():
         if row['type']=='withdrawal':
             print("\t",options.externalaccount,"\t\t",row['asset'],np.format_float_positional(-row['amount'],trim='-'))
 
-
-# for id in trade_refids:
-#     subframe=df.loc[df['refid'] == id]
-# #    print(subframe)
-#     date=parse(subframe.iloc[0]['time'])
-#     print(date.strftime('%Y/%m/%d'),"* Trade")
-#     for index, value in subframe.iloc[0].items() :
-#         print("\t; ",index,": ",value)
-#     for index, value in subframe.iloc[1].items() :
-#         print("\t; ",index,": ",value)
-#     print("\t",options.krakenaccount,"\t\t",subframe.iloc[0]['asset'],np.format_float_positional(subframe.iloc[0]['amount'],trim='-'))
-#     print("\t",options.krakenaccount,"\t\t",subframe.iloc[1]['asset'],np.format_float_positional(subframe.iloc[1]['amount'],trim='-'))
-#     print("\t",options.feeaccount,"\t\t",subframe.iloc[0]['asset']," ",np.format_float_positional(subframe.iloc[0]['fee'],trim='-'))
-#     print("\t",options.feeaccount,"\t\t",subframe.iloc[1]['asset']," ",np.format_float_positional(subframe.iloc[1]['fee'],trim='-'))
 
